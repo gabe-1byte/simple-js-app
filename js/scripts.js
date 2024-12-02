10cfe5fa009d2119b5760bba0ff6a99ccd8a41dc
gabe-1byte <gabe.m.buckman@gmail.com>
2024-12-02 11:09:51 -0700

    Revert "create pokedex list of three"
    This reverts commit 9f9f7a3fe91962fc6698acf4accc4cbfa41c4f1b.

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-let pokemonList = [
-    {name: 'Charizard', height: 67, type:['fire','flying']},
-    {name: 'Diglett', height: 8, type:['ground', 'steel']},
-    {name: 'Magnemite', height: 12, type:['electric','steel']}
-];

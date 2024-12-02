fe823931aac2fb825140ae529f7b019a40350289
gabe-1byte <gabe.m.buckman@gmail.com>
2024-12-02 11:15:44 -0700

    create pokemon and pokemon
    3 pokemon added

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Charizard', height: 67, type:['fire','flying']},
+    {name: 'Diglett', height: 8, type:['ground', 'steel']},
+    {name: 'Magnemite', height: 12, type:['electric','steel']}
+];

32f477459ec00b17aee83f6b8ff0221fda2ec586
gabe-1byte <gabe.m.buckman@gmail.com>
2024-12-04 16:02:10 -0700

    add for loop
    Loop writes the Pokemon name and height on the document.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ let pokemonList = [
     {name: 'Diglett', height: 8, type:['ground', 'steel']},
     {name: 'Magnemite', height: 12, type:['electric','steel']}
 ];
+
+//Pokemon length and height loop
+let heightMessage;
+
+for(let i = 0; i < pokemonList.length; i++){
+    if(pokemonList[i].height > 12){heightMessage = '- Wow, thats big!';
+    } else {
+        heightMessage = '';
+    }
+    document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) ${heightMessage}
+        <br> <br>`);
+}

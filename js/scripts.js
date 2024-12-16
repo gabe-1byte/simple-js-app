6349fb1d4e2d7ed5fa9ccfa21c5493d79bdb29a1
gabe-1byte <gabe.m.buckman@gmail.com>
2024-12-16 14:03:21 -0700

    add an add function + IIFE access
    I added an add function and made pokemonList accessible in the forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     {name: 'Charizard', height: 67, type:['fire','flying']},
     {name: 'Diglett', height: 8, type:['ground', 'steel']},
     {name: 'Magnemite', height: 12, type:['electric','steel']}
 ];
 
+return {
+    add: function(pokemon) {
+        pokemonList.push(pokemon);
+    },
+    getAll: function() {
+        return pokemonList;
+    }
+};
+})();
+
+pokemonRepository.add({ name: 'Squirtle', height: 20, type:['water']});
+
 //Pokemon length and height loop
 let heightMessage;
 
-function pokemonLoopFunction(pokemonList) {
-    if(pokemonList.height > 12){heightMessage = "- Wow, that's big!";
+let pokemon = pokemonRepository.getAll();
+
+function pokemonLoopFunction(pokemon) {
+    if(pokemon.height > 20){heightMessage = "- Wow, that's big!";
     } else {
         heightMessage = '';
     }
-    document.write('<p>' + `${pokemonList.name}: ${pokemonList.height}` + heightMessage + '</p>')
+    document.write('<p>' + `${pokemon.name}: ${pokemon.height}` + heightMessage + '</p>')
 }
-pokemonList.forEach(pokemonLoopFunction)
+pokemon.forEach(pokemonLoopFunction)
+    

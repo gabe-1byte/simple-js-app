5062b24b1a833867a8b6db3f0dd7f9a23b0c5566
gabe-1byte <gabe.m.buckman@gmail.com>
2024-12-17 11:03:59 -0700

    1.6 DOM edits CF

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
+//IIFE Array start
 let pokemonRepository = (function () {
     let pokemonList = [
     {name: 'Charizard', height: 67, type:['fire','flying']},
     {name: 'Diglett', height: 8, type:['ground', 'steel']},
     {name: 'Magnemite', height: 12, type:['electric','steel']}
-];
+    ];
 
-return {
-    add: function(pokemon) {
-        pokemonList.push(pokemon);
-    },
-    getAll: function() {
+    function getAll() {
         return pokemonList;
     }
-};
-})();
+
+    function add(pokemon) {
+        {
+            pokemonList.push(pokemon);
+        }
+    }
+    // Function to create pokemon details w/ button displaying name
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name
+        button.classList.add('button-class')
+        listPokemon.appendChild(button);
+        addEventListenerButton(button, pokemon);
+        pokemonList.appendChild(listPokemon);
+    }
+    // Function that displays pokemon details when clicked
+    function addEventListenerButton(button, pokemon){
+        button.addEventListener('click', function (){
+            showDetails(pokemon);
+        });
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+    // Make accessible outside function
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+    };
+})(); //IIFE array end
 
 pokemonRepository.add({ name: 'Squirtle', height: 20, type:['water']});
 
@@ -22,12 +51,6 @@ let heightMessage;
 
 let pokemon = pokemonRepository.getAll();
 
-function pokemonLoopFunction(pokemon) {
-    if(pokemon.height > 20){heightMessage = "- Wow, that's big!";
-    } else {
-        heightMessage = '';
-    }
-    document.write('<p>' + `${pokemon.name}: ${pokemon.height}` + heightMessage + '</p>')
-}
-pokemon.forEach(pokemonLoopFunction)
-    
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
